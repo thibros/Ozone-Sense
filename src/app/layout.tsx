3c7e42105cac8752281a5602ef3ac906e133e88f
thibros <git@thibros.com>
2026-03-16 04:27:28 +1400

    Update layout.tsx
    honakpuro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'OzoneSense | Ozone Concentration Calculator',
+  title: 'OzoneSense | Honkapuro Ozone Concentration Calculator',
   description: 'Calculate ozone concentration in enclosed spaces with precise modeling and scheduling.',
 };
 
